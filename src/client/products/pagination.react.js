03f67492eb97c682828519ef37ec94d7aed38dfa
Ruben Oostinga <roostinga@xebia.com>
2015-04-26 19:12:31 +0200

    pagination links

--- src/client/products/pagination.react.js
+++ src/client/products/pagination.react.js
@@ -1,13 +1,59 @@
 import React from 'react';
 import exposeRouter from '../components/exposerouter.react';
+import PureComponent from '../components/purecomponent.react';
 import {getTotal} from '../products/store'
 
 class Pagination extends React.Component {
   render() {
-    const page = this.props.router.getCurrentQuery().page || 1;
-    return (<div>{page}/{getTotal()}</div>)
+    const query = this.props.router.getCurrentQuery();
+    const page = query.page ? parseInt(query.page) : 1;
+    return <PaginationLinks page={page} total={getTotal()}/>;
+  }
+}
+
+
+const pagesAround = 4;
+const steps = 10;
+const productPerPage = 48;
+
+class PaginationLinks extends PureComponent {
+  hrefForPage(page) {
+    const router = this.props.router;
+    const path = router.getCurrentPathname();
+    const params = router.getCurrentParams();
+    const query = router.getCurrentQuery();
+    query.page = page;
+    return router.makeHref(path, params, query);
+  }
+  getPages(page, total) {
+    const pages = [];
+    for(var i = 1; i <= total; i++) {
+      if (
+        i === 1 ||
+        i % steps === 0 ||
+        page - pagesAround <= i && i <= page + pagesAround ||
+        i === total) {
+        pages.push(i);
+      }
+    }
+    return pages;
   }
 
+  render() {
+    const totalPages = Math.ceil(this.props.total / productPerPage);
+    const page = this.props.page;
+    return <div>{this.getPages(page, totalPages).map((page) =>
+    <a style={{paddingLeft: '1em', paddingRight: '1em'}}href={this.hrefForPage(page)}>{page}</a>
+    )}</div>;
+  }
 }
+
+PaginationLinks = exposeRouter(PaginationLinks);
+
+PaginationLinks.propTypes = {
+  total: React.PropTypes.number.isRequired,
+  page: React.PropTypes.number.isRequired
+};
+
 
 export default exposeRouter(Pagination);
