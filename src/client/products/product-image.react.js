8f4ecbb8c4598af33d38b3c808b194ea2c8a1726
Ruben Oostinga <roostinga@xebia.com>
2015-05-05 16:34:22 +0200

    Moved image logic to product image component

--- src/client/products/product-image.react.js
+++ src/client/products/product-image.react.js
@@ -3,13 +3,17 @@ import PureComponent from '../components/purecomponent.react';
 
 export default class ProductImage extends PureComponent {
   render() {
+    const productNumber = this.props.productNumber;
+    const normalizedName = this.props.normalizedName;
+    const src = 'https://assets.wehkamp.com/i/wehkamp/' + productNumber + '_pb_01/' + normalizedName + '.jpg?$product300x300$';
     return (
-      <img className='img-responsive' src={this.props.src} alt={this.props.title}/>
+      <img className='img-responsive' src={src} alt={this.props.alt}/>
     );
   }
 }
 
 ProductImage.propTypes = {
-  src: React.PropTypes.string.isRequired,
-  title: React.PropTypes.string.isRequired
+  productNumber: React.PropTypes.string.isRequired,
+  normalizedName: React.PropTypes.string.isRequired,
+  alt: React.PropTypes.string.isRequired
 };
--- src/client/products/product-tile.react.js
+++ src/client/products/product-tile.react.js
@@ -8,11 +8,10 @@ export default class ProductTile extends PureComponent {
     const title = this.props.product.get('title');
     const productNumber = this.props.product.get('productNumber');
     const normalizedName = this.props.product.get('normalizedName');
-    const src = 'https://assets.wehkamp.com/i/wehkamp/' + productNumber + '_pb_01/' + normalizedName + '.jpg?$product300x300$';
     return (
       <div style={{textAlign: 'center'}} className='col-xs-6 col-sm-4 col-md-3 col-lg-3'>
         <div>{title}</div>
-        <div><ProductImage src={src} title={this.props.product.get('title')}/></div>
+        <div><ProductImage productNumber={productNumber} normalizedName={normalizedName} alt={title}/></div>
       </div>
     );
   }
